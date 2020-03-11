70e94740df093514cf8fae8a15f76ed4fac8d17d
dhephersonRibeiro <dhephmir@gmail.com>
2020-03-11 10:07:03 -0300

    feat: add validation

--- Server/index.js
+++ Server/index.js
@@ -5,14 +5,33 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+
 app.get('/', (req,res)=>{
     res.json({
         "message":"funfando"
     })
 })
 
+function isValidDweet(Dweet){
+    return Dweet.name && Dweet.name.toString().trim() !== '' &&
+    Dweet.content && Dweet.content.toString().trim() !== ''
+}
 app.post('/Dweets',(req,res)=>{
-    console.log(req.body)
+    if(isValidDweet(req.body)){
+        //insert into database
+        const Dweet = {
+            name : req.body.name.toString(),
+            content: req.body.content.toString()
+        };
+        
+        console.log(Dweet)
+
+    }else {
+        res.status(422)
+        res.json({
+            "message":"preencha os campos corretamente!"
+        })
+    }
 })
 app.listen(5000, () =>{ 
     console.log("Listening on http://localhost:5000")
